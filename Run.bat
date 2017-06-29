ac3330097133a77c8d25f959d956be96bbb794bf
nwfxyz <ngweifeng@u.nus.edu>
2017-06-29 15:02:38 +0800

    edits

--- Run.bat
+++ Run.bat
@@ -1,3 +1,2 @@
-cd C:\Users\NgWei\OneDrive\Documents\Python Scripts\Wallet
 python WalletPython.py
 pause
--- WalletPython.py
+++ WalletPython.py
@@ -12,7 +12,7 @@ import pickle
 
 formatter = logging.Formatter('%(asctime)s,%(levelname)s,%(message)s')
 
-os.chdir(r"C:\Users\NgWei\OneDrive\Documents\Python Scripts\Wallet")
+
 
 CREATE2,CREATE3,CREATE4,CREATE5 = range(4)
 curList = {}
@@ -67,9 +67,17 @@ def start(bot,update):
            )
     return ConversationHandler.END
 
-
+def completelisting(userid,ListID):
+    List_dir = "Processing\\user" + str(userid)
+    stging_list = load_object(List_dir + '\\' + ListId + '.pkl')
+    Title = stging_list.Title
+    Desc = stging_list.Desc
+    
+    
+    
   
-def create(bot,update):
+def create(bot,update,user_data):
+    user_data['id'] = update.message.from_user.id
     keyboard = [[InlineKeyboardButton("Stock", callback_data='Stock')],
                  [InlineKeyboardButton("Rental Space", callback_data='Rental')],
                 [InlineKeyboardButton("Others", callback_data='Others')]]
@@ -114,11 +122,9 @@ def NewFolder(FolderName):
 
     
 def NewListing(bot,update,user_data):
-    global curList
-    print(user_data)
+    
     query = update.callback_query
-    user = query.message.from_user
-    userid = user.id
+    userid = user_data['id']
     List_dir = "Processing\\user" + str(userid)
     NewFolder(List_dir)
     Prev_Listing = os.listdir(List_dir)
@@ -127,26 +133,59 @@ def NewListing(bot,update,user_data):
     else:
         ListId = str(max([int( re.sub('\.pkl','',i)) for i in Prev_Listing if 'pkl' in i]) + 1).zfill(4)
         
-    curList[userid] = ListId
+    user_data['curr'] = ListId
     bot.edit_message_text(message_id=query.message.message_id,chat_id = query.message.chat_id,
-                          text = "ListID =" + str(userid) + str(curList[userid]) + "\nType:" + query.data + "\nPlease give us a listing title")
-    ListObj = Listing(query.data)
-    save_object(ListObj,List_dir + '\\' + ListId + '.pkl')
+                          text = "ListID =" + str(userid) + str(ListId) + "\nType:" + query.data + "\nPlease give us a listing title")
+    stging_list = Listing(query.data)
+    save_object(stging_list,List_dir + '\\' + ListId + '.pkl')
     return CREATE3
      
-def AddTitle(bot,update):
-    global curList
+def AddTitle(bot,update,user_data):
+    
     user = update.message.from_user
     userid = user.id
-    print(userid)
-    print(curList)
-    List_dir = "Processing\\user" + str(userid)
-    ListId = curList[userid] 
-    stging_list = load_object(List_dir + '\\' + ListId + '.pkl')
-    stging_list.Title = update.message.text
-    update.message.reply_text('Add a description',
-                              reply_markup=ReplyKeyboardRemove())
     
+  
+    
+    if len(str(update.message.text)) < 50 :
+          List_dir = "Processing\\user" + str(userid)
+          ListId = user_data['curr']
+          stging_list = load_object(List_dir + '\\' + ListId + '.pkl')
+          stging_list.Title = update.message.text
+          update.message.reply_text('Add a description',
+                                    reply_markup=ReplyKeyboardRemove())
+          save_object(stging_list,List_dir + '\\' + ListId + '.pkl')
+          return CREATE4
+    else: 
+        update.message.reply_text('Title too long, max 50 char',
+                                    reply_markup=ReplyKeyboardRemove())
+        return CREATE3
+    
+def AddDesc(bot,update,user_data):
+    
+    user = update.message.from_user
+    userid = user.id
+    
+  
+    
+    if len(str(update.message.text)) < 1500 :
+          List_dir = "Processing\\user" + str(userid)
+          ListId = user_data['curr']
+          stging_list = load_object(List_dir + '\\' + ListId + '.pkl')
+          stging_list.Desc = update.message.text
+          update.message.reply_text('Done. Listing added',
+                                    reply_markup=ReplyKeyboardRemove())
+          save_object(stging_list,List_dir + '\\' + ListId + '.pkl')
+          
+          return CREATE5
+    else: 
+        update.message.reply_text('Desc too long, max 1500 char',
+                                    reply_markup=ReplyKeyboardRemove())
+        return CREATE4
+    
+
+
+
 def main():
     # Create the EventHandler and pass it your bot's token.
     updater = Updater("355993039:AAFkJmcjKgQLWz3IWBGz1niXLFq8zq1vH6M")
@@ -156,14 +195,14 @@ def main():
 
     # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
     conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('create', create),
+        entry_points=[CommandHandler('create', create,pass_user_data = True),
                  
                       CommandHandler('help', help1)
                       ],
 
         states={
                 CREATE2: [CallbackQueryHandler(NewListing,pass_user_data = True)],
-                CREATE3: [MessageHandler(Filters.text,AddTitle)]
+                CREATE3: [MessageHandler(Filters.text,AddTitle,pass_user_data = True)]
                 
         },
 
